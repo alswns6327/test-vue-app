b9705ea8de91ea2daf7de579f51166cb8b01eac6
owner <alswns6327@naver.com>
2023-11-08 17:19:17 +0900

    $emit

---
+++ src/App2.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <InputName @changeName="changeNameHandler"/>
+    </div>
+    <br/>
+    <span>parent name : {{ parentName }}</span>
+    <span>num : {{ num }}</span>
+</template>
+
+<script>
+    import InputName from '@/components/InputName.vue';
+    export default {
+        name : "App2",
+        data(){
+            return {
+                parentName : "1212", num : '1'
+            }
+        },
+        methods : {
+            changeNameHandler(name, num){
+                this.parentName = name;
+                this.num = num;
+            }
+        },
+        components : {InputName}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/InputName.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        이름 : <input type="text" v-model="name"/>
+        <button @click="$emit('changeName', name, '3')">event send</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "InputName",
+        data(){
+            return {
+                name : ""
+            }
+        },
+        // emits : ["changeName"]
+        emits : {
+            changeName :  (name, num) => {
+                console.log(name);
+                console.log(num);
+                return true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
